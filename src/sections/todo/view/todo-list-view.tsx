b1b75226698b38e99dca14390a4f46674c89a40e
Revti-Kale <revtiikale@gmail.com>
2025-01-11 17:36:13 +0530

    implemented add todo functionality

--- src/sections/todo/view/todo-list-view.tsx
+++ src/sections/todo/view/todo-list-view.tsx
@@ -10,6 +10,9 @@ import Typography from "@mui/material/Typography";
 import { useState } from "react";
 import { useSelector } from "react-redux";
 import ClearIcon from "@mui/icons-material/Clear";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import TextareaAutosize from "@mui/material/TextareaAutosize";
 
 const myTodos = [
   {
@@ -46,6 +49,9 @@ const myTodos = [
 
 export default function ToDoListView() {
   const [todos, setTodos] = useState(myTodos);
+  const [showForm, setShowForm] = useState(false);
+  const [newTitle, setNewTitle] = useState("");
+  const [newDescription, setNewDescription] = useState("");
 
   const searchTerm = useSelector((state: RootState) =>
     state.search.searchTerm.toLowerCase()
@@ -59,8 +65,51 @@ export default function ToDoListView() {
     setTodos(updatedTodos);
   }
 
+  function handleAddTodo() {
+    if (newTitle.trim() && newDescription.trim()) {
+      const newTodo = {
+        title: newTitle,
+        description: newDescription,
+      };
+      setTodos([...todos, newTodo]);
+      setNewTitle("");
+      setNewDescription("");
+      setShowForm(false);
+    }
+  }
+
   return (
     <Container maxWidth="lg" sx={{ marginTop: 4 }}>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => setShowForm(!showForm)}
+        sx={{ marginBottom: 2 }}
+      >
+        {showForm ? "Cancel" : "Add Todo"}
+      </Button>
+      {showForm && (
+        <Box sx={{ marginBottom: 2 }}>
+          <TextField
+            label="Title"
+            variant="outlined"
+            fullWidth
+            value={newTitle}
+            onChange={(e) => setNewTitle(e.target.value)}
+            sx={{ marginBottom: 2 }}
+          />
+          <TextareaAutosize
+            minRows={4}
+            placeholder="Description"
+            style={{ width: "100%", padding: "8px", marginBottom: "16px" }}
+            value={newDescription}
+            onChange={(e) => setNewDescription(e.target.value)}
+          />
+          <Button variant="contained" onClick={handleAddTodo}>
+            Add Todo
+          </Button>
+        </Box>
+      )}
       <Grid container spacing={3} justifyContent="center">
         {filteredTodos.map((todo, index) => (
           <Grid item xs={12} sm={6} md={4} key={index}>
